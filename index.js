76c44218a7544b070ef2bd075828d4c9c47f880c
Sti3v <sti3v355@gmail.com>
2023-02-27 22:12:06 +0100

    ajouter le gif pour chaque image presente

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 function swapImageAndGif() {
   const images = document.querySelectorAll(".swap-element");
+  let timeout;
+
   images.forEach((image) => {
     image.addEventListener("mouseenter", () => {
       const gif = image.getAttribute("data-gif");
-      Velocity(
-        image,
-        { scale: 1.2 },
-        {
-          duration: 200,
-          easing: "ease-out",
-          complete: () => {
-            image.setAttribute("src", gif);
-          },
-        }
-      );
+      timeout = setTimeout(() => {
+        image.style.transition = "transform 0.2s ease-out";
+        Velocity(
+          image,
+          { scale: 1.2 },
+          {
+            duration: 200,
+            easing: "ease-out",
+            complete: () => {
+              image.setAttribute("src", gif);
+            },
+          }
+        );
+      }, 500);
     });
     image.addEventListener("mouseleave", () => {
+      clearTimeout(timeout);
       const src = image.getAttribute("src");
       const img = image.getAttribute("data-img");
       if (src !== img) {
+        image.style.transition = "transform 0.2s ease-out";
         Velocity(
           image,
           { scale: 1.0 },
@@ -34,3 +41,16 @@ function swapImageAndGif() {
     });
   });
 }
+
+swapImageAndGif();
+
+function preloadImages() {
+  const images = document.querySelectorAll(".swap-element");
+  for (let i = 0; i < images.length; i++) {
+    const gif = images[i].getAttribute("data-gif");
+    const img = new Image();
+    img.src = gif;
+  }
+}
+
+preloadImages();
